b718049666026b6f898e9ffbcdbb9df9fb45d0e3
Michael Todd <mikejtodd@gmail.com>
2014-05-09 15:44:49 -0500

    File structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeHearNebraska::Application.config.secret_token = '317c0bcbe8c0534b54004ac49508d804c1cfb32c032438c44f9d8dcb1876c6a656afded336a2ca8ca3d166c1813ab9c282cf99b1d25dca3945cfdd0668799b88'
