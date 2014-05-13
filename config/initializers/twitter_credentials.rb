d28ee4d63466d921ee795aebdfe5e4e959ec835d
Michael Todd <mikejtodd@gmail.com>
2014-05-13 18:56:53 -0500

    Moved Twitter config into initializer

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,6 +1,6 @@
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "j1p3ea1o39kXFPCb5q7J9cWHl"
-  config.consumer_secret     = "u9XPWkNeMRtQ9xwoJLz7TNoJhXBkyhHrWMMNg3T2mp0Gyg9YyS"
-  config.access_token        = "109768622-DyE4DRXLn29uE7BwTXUAQjhUVpLEqQruYNIef85x"
-  config.access_token_secret = "QTWnTEOC9BqbpJC2at1yCvIaRUpSJ6GqmOqasgQ3lk2fR"
+require 'twitter'
+
+Twitter.configure do |config|
+  config.twitter_key = "j1p3ea1o39kXFPCb5q7J9cWHl"
+  config.twitter_secret = "u9XPWkNeMRtQ9xwoJLz7TNoJhXBkyhHrWMMNg3T2mp0Gyg9YyS"
 end
