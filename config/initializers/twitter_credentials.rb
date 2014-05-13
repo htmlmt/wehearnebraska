2ca504e8e72d3cd1349c94ea9defe1a8697843a9
Michael Todd <mikejtodd@gmail.com>
2014-05-13 18:58:45 -0500

    Edits to Twitter config

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,6 +1,4 @@
-require 'twitter'
-
-Twitter.configure do |config|
+client = Twitter::REST::Client.new do |config|
   config.twitter_key = "j1p3ea1o39kXFPCb5q7J9cWHl"
   config.twitter_secret = "u9XPWkNeMRtQ9xwoJLz7TNoJhXBkyhHrWMMNg3T2mp0Gyg9YyS"
 end
