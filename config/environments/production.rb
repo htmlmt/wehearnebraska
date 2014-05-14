be11ffbbfe9f2cf96be74681ff540d91a0630049
Michael Todd <mikejtodd@gmail.com>
2014-05-13 23:07:23 -0500

    Now displays tweets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 WeHearNebraska::Application.configure do
-  config.twitter_key = "j1p3ea1o39kXFPCb5q7J9cWHl"
-  config.twitter_secret = "u9XPWkNeMRtQ9xwoJLz7TNoJhXBkyhHrWMMNg3T2mp0Gyg9YyS"
+  
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
