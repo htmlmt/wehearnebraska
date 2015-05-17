98b774bb9f56bc4a8b39d18faeda2ab2cce8c6e0
Michael Todd <mikejtodd@gmail.com>
2015-05-17 15:21:46 -0500

    Added stories, link underline to photo credits

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var photoInterval = setInterval(changePhoto, 8000);
     var photoPositionArray = [0, 25, 50, 75, 100];
-    var photoCredits = ["photo of The Avett Brothers by Matthew Masin", "photo of Sleater-Kinney by Chris Dinan", "photo of Shabazz Palaces by Chris Dinan", "photo of The Menzingers by Andrew Norman", "photo of Skrillex by Will Stott"]
+    var photoCredits = ["photo of <a href=\"http://hearnebraska.org/feature/avett-brothers-pershing-center-photo-slideshow/\" target=\"_blank\">The Avett Brothers</a> by Matthew Masin", "photo of <a href=\"http://hearnebraska.org/feature/sleater-kinney-slowdown-photos-review/\" target=\"_blank\">Sleater-Kinney</a> by Chris Dinan", "photo of <a href=\"http://hearnebraska.org/feature/shabazz-palaces-spencelove-vega-concert-review-photos/\" target=\"_blank\">Shabazz Palaces</a> by Chris Dinan", "photo of <a href=\"http://hearnebraska.org/feature/menzingers-flatliners-broadway-calls-photo-essay/\" target=\"_blank\">The Menzingers</a> by Andrew Norman", "photo of <a href=\"http://hearnebraska.org/feature/skrillex-streets-photo-review/\" target=\"_blank\">Skrillex</a> by Will Stott"]
     var photoIndex = 1;
     function changePhoto() {
         $('.container--header').css('background-position', '' + photoPositionArray[photoIndex] + '% 0%');
